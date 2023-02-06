466f0a42b6d0c3b13a7b8b55b8321216dfd2d0c9
adityabhoir777 <adityabhoir@mailfence.com>
2023-02-06 09:44:49 +0530

    assignment created

---
+++ index.js
@@ -0,0 +1,13 @@
+// 3
+// Import functions move and changeColor from animate.js
+
+document.getElementById('btn').addEventListener('click', () => {
+
+
+    // 4 
+    // call move here
+
+
+    //5
+    // call changeColor here
+})
---
+++ utils/animate.js
@@ -0,0 +1,15 @@
+function move() {
+    document.getElementById('mul').style.marginTop = "-90px";
+    document.getElementById('add').style.marginTop = "60px";
+    document.getElementById('add').style.marginLeft = "-70px";
+    document.getElementById('sub').style.marginTop = "60px";
+    document.getElementById('sub').style.marginLeft = "70px";
+}
+
+function changeColor() {
+    document.getElementById('btn').style.backgroundColor = "yellow";
+}
+
+
+// 2
+// Do a named export here to export both of the above functions
